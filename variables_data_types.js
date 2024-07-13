bba07493d6afe9ae8c2db962539e7dce1777bacd
Linuso77 <dharmenderchugh71@gmail.com>
2024-07-14 01:09:48 +0530

    Initial commit

---
+++ variables_data_types.js
@@ -0,0 +1,39 @@
+// Task 1
+var num = 7;
+console.log(num);
+// Task 2
+let str1 = "linus";
+console.log(str1);
+// Task 3
+const bool = true
+console.log(bool)
+// Task 4
+let num1 = 3;
+console.log(typeof(num1))
+let str2 = "hello linus";
+console.log(typeof(str2))
+let bool1 = false
+console.log(typeof(bool1))
+let obj = {
+    "name":"linus",
+    "challenge":"30 days js challenge"
+}
+console.log(typeof(obj))
+let arr = [1,2,3,4]
+console.log(typeof(arr)) // object
+// Task 5
+console.log(num+" Intial value")
+num = 9;
+console.log(num+" value after change")
+// Task 6
+
+const num2 = 2
+console.log(num2,"inital value")
+
+try {  
+    num2 = 3
+    console.log(num2)    
+ }  
+ catch (e) {  
+   console.log(e.message); // This will generate an error message  
+ }
